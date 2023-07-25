b94ece5d4b0b92b56bfaf68e3ac35ea21e7ecc00
Francisco Abarzua <git.id@frabarz.cl>
2023-07-25 17:15:30 -0400

    updates test for rca to new fixture

--- tests/test_rca.py
+++ tests/test_rca.py
@@ -5,28 +5,29 @@ import economic_complexity as ecplx
 
 
 def test_rca(df_exports: pd.DataFrame):
-    pv_exports = pd.pivot_table(df_exports, values="Trade Value",
-                                            index="Country",
-                                            columns="HS4 ID",
-                                            aggfunc=np.sum)
+    pv_exports = pd.pivot_table(df_exports,
+                                values="Trade Value",
+                                index="Exporter Country ID",
+                                columns="Section ID",
+                                aggfunc=np.sum)
     rca = ecplx.rca(pv_exports)
 
-    hs4_0201 = rca[10201].describe().round(6)
-    assert hs4_0201["count"] == 226
-    assert hs4_0201["mean"] == 1.076052
-    assert hs4_0201["std"] ==  4.715384
-    assert hs4_0201["min"] ==  0.000000
-    assert hs4_0201["25%"] ==  0.000000
-    assert hs4_0201["50%"] ==  0.000293
-    assert hs4_0201["75%"] ==  0.094575
-    assert hs4_0201["max"] == 44.864227
+    section_1 = rca[1].describe().round(6)
+    assert section_1["count"] == 226
+    assert section_1["mean"] == 3.484895
+    assert section_1["std"] ==  8.261088
+    assert section_1["min"] ==  0.000000
+    assert section_1["25%"] ==  0.129100
+    assert section_1["50%"] ==  0.718371
+    assert section_1["75%"] ==  2.165564
+    assert section_1["max"] == 47.724923
 
-    hs4_9615 = rca[209615].describe().round(6)
-    assert hs4_9615["count"] == 226
-    assert hs4_9615["mean"] == 0.223670
-    assert hs4_9615["std"]  == 0.695201
-    assert hs4_9615["min"]  == 0.000000
-    assert hs4_9615["25%"]  == 0.000033
-    assert hs4_9615["50%"]  == 0.023735
-    assert hs4_9615["75%"]  == 0.146443
-    assert hs4_9615["max"]  == 6.930755
+    section_9 = rca[9].describe().round(6)
+    assert section_9["count"] == 226
+    assert section_9["mean"] == 1.959028
+    assert section_9["std"]  == 6.469812
+    assert section_9["min"]  == 0.000000
+    assert section_9["25%"]  == 0.040933
+    assert section_9["50%"]  == 0.267903
+    assert section_9["75%"]  == 1.264129
+    assert section_9["max"]  == 75.963223
