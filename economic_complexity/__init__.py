cf79b4f641faae62504f175b3917abbb6339b0ac
Francisco Abarzua <git.id@frabarz.cl>
2023-10-10 17:29:23 -0300

    v0.2.0

--- economic_complexity/__init__.py
+++ economic_complexity/__init__.py
@@ -9,7 +9,7 @@ from .product_space import distance, opportunity_gain, proximity, relatedness, s
 from .cross_space import cross_proximity, cross_relatedness
 from .subnational import complexity_subnational
 
-__version_info__ = ('0', '1', '4')
+__version_info__ = ('0', '2', '0')
 __version__ = '.'.join(__version_info__)
 
 __all__ = (
