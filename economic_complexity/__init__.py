a11e1af2591ff0210905a4f2b4c3692d92eb0640
Francisco Abarzua <git.id@frabarz.cl>
2024-02-19 17:52:22 -0300

    updates CI workflow and lockfile

--- economic_complexity/__init__.py
+++ economic_complexity/__init__.py
@@ -3,14 +3,22 @@
 This module contains functions to ease the calculation of Economic Complexity values.
 """
 
-from .rca import rca
 from .complexity import complexity
-from .product_space import distance, opportunity_gain, proximity, relatedness, similarity, pgi, peii
 from .cross_space import cross_proximity, cross_relatedness
+from .product_space import (
+    distance,
+    opportunity_gain,
+    peii,
+    pgi,
+    proximity,
+    relatedness,
+    similarity,
+)
+from .rca import rca
 from .subnational import complexity_subnational
 
-__version_info__ = ('0', '2', '0')
-__version__ = '.'.join(__version_info__)
+__version_info__ = ("0", "2", "2")
+__version__ = ".".join(__version_info__)
 
 __all__ = (
     "rca",
@@ -24,5 +32,5 @@ __all__ = (
     "similarity",
     "pgi",
     "peii",
-    "complexity_subnational"
+    "complexity_subnational",
 )
